741fc9ccbe626d4883115641cca5029ce02dfedc
glen <163131008+gleneith@users.noreply.github.com>
2024-09-16 10:24:54 +0100

    Update pizza database.sql

--- pizza database.sql
+++ pizza database.sql
@@ -138,3 +138,72 @@ CREATE TABLE `rota` (
         - Staff shift duration 
         - Staff cost per role
 */
+-- showing the order activities within the pizzeria
+    select 
+    o.order_id, i.item_price, o.quantity, i.item_cat, i.item_name, o.created_at,
+    a.delivery_address1, a.delivery_address2, a.delivery_city, a.delivery_zipcode, 
+    o.delivery
+    from orders o 
+    left join item i on o.item_id = i.item_id
+    left join address a on o.add_id - a.add_id
+
+-- showing the total quantity by the ingredients 
+    select 
+    o.item_id, i.sku, i.item_name, 
+    sum(o.quantity) as order_quantity
+    from orders o
+    left join item i on o.item_id = i.item_id
+    group by o.item_id, i.sku, i.item_name
+
+-- showing the cost of ingredients
+    select 
+    s1.item_name, s1.ing_id, s1.ing_name, s1.ing_weight, s1.ing_price, s1.order_quantity, s1.recipe_quantity,
+    s1.order_quantity * s1.recipe_quantity as ordered_weight,
+    s1.ing_price/s1.ing_weight as cost_of_unit,
+    (s1.order_quantity * s1.recipe_quantity) * (s1.ing_price / s1.ing_weight) as cost_of_ingredient
+    
+    from (select 
+    o.item_id, i.sku, i.item_name, r.ing_id,ing.ing_name,ing.ing_weight, ing.ing_price,
+    r.quantity as recipe_quantity,
+    sum(o.quantity) as order_quantity
+    from orders o
+    left join item i on o.item_id = i.item_id
+    left join recipe r on i.sku = r.recipe_id
+    left join ingredient ing on ing.ing_id = r.ing_id
+    group by o.item_id, i.sku, i.item_name, r.ing_id, r.quantity, ing.ing_name,ing.ing_weight, ing.ing_price) s1
+
+-- showing the calculated cost of each pizza and the remaining stock by ingredients. 
+/* note:: the results from  the previous query were saved as a view allowing me to work on analysing this task. which was saved as "stock one".*/
+    select 
+    s2.ing_name, s2.ordered_weight, 
+    ing.ing_weight * inv.quantity as total_inv_weight,
+    (ing.ing_weight * inv.quantity) - s2.ordered_weight as remaing_weight
+    from (select 
+    ing_id, ing_name, sum(ordered_weight) as ordered_weight
+    from stock_one
+    group by ing_name, ing_id, ordered_weight)s2
+    
+    left join inventory inv on inv.item_id = s2.ing_id
+    left join ingredient ing on ing.ing_id = s2.ing_id
+
+-- showing the staff shift duration
+    select 
+    r.date,
+    s.first_name, s.last_name,
+    sh.start_time, sh.end_time,
+    time(timediff(sh.end_time,sh.start_time)) as shift_duration
+    from rota r
+    left join staff s on r.staff_id = s.staff_id
+    left join shift sh on r.shift_id  = sh.shift_id
+
+-- showing how much each staff is being paid based on their role and shift duration
+    select 
+    r.date,
+    s.first_name, s.last_name,
+    sh.start_time, sh.end_time,
+    time(timediff(sh.end_time,sh.start_time)) as shift_duration,
+    ((hour(timediff(sh.end_time, sh.start_time)) * 60) + (minute(timediff(sh.end_time, sh.start_time)))) / 60 as hours_in_shift,
+    ((hour(timediff(sh.end_time, sh.start_time)) * 60) + (minute(timediff(sh.end_time, sh.start_time)))) / 60 * s.hourly_rate as staff_cost
+    from rota r
+    left join staff s on r.staff_id = s.staff_id
+    left join shift sh on r.shift_id  = sh.shift_id
